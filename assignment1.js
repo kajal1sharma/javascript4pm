f068dd0acebb07fcb0db871fde4211801966c921
brainmentorspvtltd <kajal1231995@gmail.com>
2023-04-10 17:25:26 +0530

    assignment 1

--- assignment1.js
+++ assignment1.js
@@ -5,21 +5,41 @@ window.addEventListener('load',()=>{
         return data.json()
     })
     .then(data=>{
-        //console.log(data);
+        console.log(data);
         let todoDiv = document.getElementById("todoItems")
         data.forEach(element => {
+
             let div= document.createElement('div')
             div.innerText =element.title;
+            let buttonnew = document.createElement('button');
+            buttonnew.innerText="close";
+            buttonnew.className="newbutton"
+            // buttonnew.style.padding="20px";
+            // buttonnew.style.backgroundColor ="pink";
+
+            div.appendChild(buttonnew);
             todoDiv.appendChild(div)
         });
     })
 
     let inputfield = document.getElementById("inp")
+    // console.log(inputfield,"----");
+    let inptval="";
+    inputfield.addEventListener('input',(event)=>{
+        inptval =event.target.value;
+         console.log(inptval)
+         
      
-    inputfield.addEventListener('change',(event)=>{
-         let inptval = event.target.value;
     })
 
-
+    
+    let button = document.getElementById('button1');
+         button.addEventListener('click',()=>{
+        console.log(inptval );
+        let todoDiv = document.getElementById("todoItems")
+        let div= document.createElement('div')
+        div.innerText = inptval;
+        todoDiv.appendChild(div)
+    })
 
 })
