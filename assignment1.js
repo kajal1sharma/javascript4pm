bcf95990cc181a728abb4265cfb8595dc4ca45f6
brainmentorspvtltd <kajal1231995@gmail.com>
2023-04-11 17:32:27 +0530

    assignment

--- assignment1.js
+++ assignment1.js
@@ -1,40 +1,58 @@
+let inptval={value:"",type:0, index:0};
+data=[{title:"all new task"},{title:"new task"}];
 window.addEventListener('load',()=>{
     console.log("window is loaded")
     fetch('https://jsonplaceholder.typicode.com/todos/')
-    .then(data=>{
-        return data.json()
+    .then(data1=>{
+        return data1.json()
     })
-    .then(data=>{
+    .then(data1=>{
         // console.log(data);
-        data=[{title:"all new task"},{title:"new task"}];
+        
         let todoDiv = document.getElementById("todoItems")
         data.forEach((element, index) => {
 
             let div= document.createElement('div')
+            div.className="divitem"
             div.innerText =element.title;
             
             let buttonnew = document.createElement('button');
+            let buttonedit =document.createElement('button');
             buttonnew.innerText="close";
-            buttonnew.className="newbutton"
-            
+            buttonedit.innerText="edit";
+            buttonnew.className="newbutton";
+            buttonedit.className="newbutton";
+            div.appendChild(buttonnew);
+            div.appendChild(buttonedit);
+            todoDiv.appendChild(div)
+            buttonedit.addEventListener('click',()=>{
+                let inputfield = document.getElementById("inp");
+                inputfield.value=element.title;
+                inptval.type=1;
+                inptval.index=index;
+                console.log(inptval,"======")
+                inputfield.autofocus;
+                
+            })
             buttonnew.addEventListener('click',()=>{
                 console.log(element,index);
                 data.splice(index,1);
                 console.log(data);
                 todoDiv.removeChild(div);
 
+
             })
 
-            div.appendChild(buttonnew);
-            todoDiv.appendChild(div)
+            
         });
     })
 
     let inputfield = document.getElementById("inp")
     // console.log(inputfield,"----");
-    let inptval="";
+    
     inputfield.addEventListener('input',(event)=>{
-        inptval =event.target.value;
+         inptval.value =event.target.value;
+                
          console.log(inptval)
          
      
@@ -42,12 +60,24 @@ window.addEventListener('load',()=>{
 
     
     let button = document.getElementById('button1');
-         button.addEventListener('click',()=>{
+         button.addEventListener('click',(event)=>{
+            console.log(event);
         console.log(inptval );
         let todoDiv = document.getElementById("todoItems")
-        let div= document.createElement('div')
-        div.innerText = inptval;
-        todoDiv.appendChild(div)
+        let divitem= document.createElement('div')
+        divitem.className="divitem";
+        console.log(inptval)
+                if(inptval.type===0)
+        {
+            div.innerText = inptval.value;
+            todoDiv.appendChild(divitem)
+        }
+        else if(inptval.type===1){
+            data[inptval.index]=inptval.value;
+            let listarr =todoDiv.getElementsByClassName('divitem')
+            console.log(listarr)
+            listarr[inptval.index].innerText=inptval.value;
+        }
     })
 
 })
