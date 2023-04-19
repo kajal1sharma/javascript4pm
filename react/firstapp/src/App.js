f2db5467d284948e6cb1261351c3d614eac1ef7a
brainmentorspvtltd <kajal1231995@gmail.com>
2023-04-19 16:42:45 +0530

    react first project

---
+++ react/firstapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Hero from "./component/Hero"
+
+function App() {
+  return (
+    <div>
+     <Hero/>
+     <div className='btn btn-primary'>
+          submit button
+     </div>
+    </div>
+  );
+}
+
+export default App;
