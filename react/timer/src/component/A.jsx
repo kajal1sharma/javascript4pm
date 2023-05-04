9c446eafeb9b7a28fdeea5f977008c19fe074891
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-04 16:40:26 +0530

    ..

--- react/timer/src/component/A.jsx
+++ react/timer/src/component/A.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import C from  "./C";
 function A({title}){
     console.log(title);
     return (
         <div style={{width:"300px", height:"300px",backgroundColor:"green"}}>
             <h1>this is Element A</h1>
             <h2>{title}</h2>
+            <C/>
         </div>
     )
 }
---
+++ react/timer/src/component/C.jsx
@@ -0,0 +1,11 @@
+function C(){
+    return (
+        <>
+            <h1>hi from C</h1>
+            <h2>hi from c again</h2>
+        </>
+        
+    )
+}
+
+export default C;
