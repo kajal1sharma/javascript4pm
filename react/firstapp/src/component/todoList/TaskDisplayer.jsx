a9998a0b912d06f2b981e691e19966c69b97f79a
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-08 17:09:15 +0530

    ..

--- react/firstapp/src/component/todoList/TaskDisplayer.jsx
+++ react/firstapp/src/component/todoList/TaskDisplayer.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 import SingleTask from './SingleTask'
 
 const TaskDisplayer = ({data}) => {
+  //let arr= [<h1>{"iam elelement 1"}</h1>,<h1>{"iam element 2"}</h1>]
   return (
     <div>
+      {/* {arr} */}
       {data.map(ele=>{
         return <SingleTask ele={ele}/>
       })}
