8e0d7407fcf4756e3de84ef1dccaabdb6730c911
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-15 16:37:57 +0530

    ..

---
+++ react/restaurant/src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+function App() {
+
+  const [flag , setFlag] = useState(true);
+ const [count, setcount] =useState(0);
+ 
+  useEffect(()=>{
+    console.log("flag value",flag);
+  },[flag])
+
+  function clickHandler(){
+    setFlag(flag?false:true);
+  }
+
+  function countHandler(){
+    setcount(count+1);
+  }
+  return (
+
+    <div><div style={{width:'300px',height:'300px',objectFit:"contain" }}>
+      {flag ===true && <img  height="300px" width={'300px'} alt="" src="https://thumbs.dreamstime.com/b/beautiful-rain-forest-ang-ka-nature-trail-36703721.jpg" />}
+      </div>
+      <button onClick={clickHandler}>Click</button>
+      <h1>{count}</h1>
+      <button onClick={countHandler}>Count Inc</button>
+    </div>
+  );
+}
+
+export default App;
