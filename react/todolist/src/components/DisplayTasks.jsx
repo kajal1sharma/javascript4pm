13b2f8520da2e3a80d9b0b8de665bc5c944b740a
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-18 16:22:57 +0530

    ..

--- react/todolist/src/components/DisplayTasks.jsx
+++ react/todolist/src/components/DisplayTasks.jsx
@@ -1,17 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const DisplayTasks = ({data, deleteHandler}) => {
+  
   return (
     <div>
       {data.map(ele=>{
         return <li>
-            {ele.name}
-            <button >Edit</button>
+            {/* {ele.name} */}
+            {<InputTask ele={ele.name}/>}
+            
             <button onClick={()=>{deleteHandler(ele.id)}}>Delete</button>
         </li>
       })}
     </div>
   )
+}
+
+function InputTask({ele}){
+  const [edit, setEdit] =useState(false);
+  if(edit===true){
+    return <div>
+      <input value={ele} />
+      <button  onClick={()=>{
+              setEdit(false);
+            }}>Save</button>
+    </div>
+  }
+  else{
+    return <div>
+      <p>{ele}</p>
+      <button  onClick={()=>{
+              setEdit(true);
+            }}>Edit</button>
+    </div>
+  }
+
 }
 
 export default DisplayTasks
