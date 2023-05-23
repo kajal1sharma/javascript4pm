ffd5fe9787d6bd58739bafcc351967802201a6a2
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-23 17:12:30 +0530

    ..

--- react/shoppingsite/src/Seventh.jsx
+++ react/shoppingsite/src/Seventh.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import userContext from './userContext'
 import { useContext } from 'react'
+import {useRef} from "react"
+
 const Seventh = () => {
 
     const user = useContext(userContext);
-
+    const divref = useRef(null);
+    useEffect(()=>{
+        console.log(divref)
+        divref.current.style.backgroundColor ="orange";
+    },[])
   return (
-    <div style={{border:'5px solid orangered' , fontSize:user.fontSize}}>
+    <div ref={divref} style={{border:'5px solid orange' , fontSize:user.fontSize}}>
       Seventh
     </div>
   )
