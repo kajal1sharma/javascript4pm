27ecbedf5a8af5b6ec57e85bc6bc23ee976d4909
brainmentorspvtltd <kajal1231995@gmail.com>
2023-05-23 17:24:28 +0530

    ..

--- react/shoppingsite/src/SecondComp.jsx
+++ react/shoppingsite/src/SecondComp.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 
 const SecondComp = () => {
+    const [rerender , setRerender] =useState(0);
+   const user= useRef({name:"rita", surname:"verma"});
+//    useState(90);
+//    let a=0;
   return (
     <div style={{border:'5px solid yellow'}}>
       Second Component
+      <button onClick={()=>{setRerender(0);console.log("rerendering",user.current.name)}}>Rerender</button>
+      <input placeholder="name" onChange={(event)=>{user.current.name=event.target.value}}/>
+      {/* {a=90} */}
     </div>
   )
 }
